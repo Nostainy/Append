3a9e3e31d634d6b0144ddd85be2103cf831b311c
olamide <atandaolamideolaadua@gmail.com>
2024-10-28 14:17:48 +0100

    first commit

---
+++ img/main.js
@@ -0,0 +1,14 @@
+"use strict";
+const olamide = document.querySelector(".hero");
+
+console.log(olamide);
+
+const horllyList = document.querySelectorAll(".list");
+
+console.log(horllyList);
+
+// const change = document.querySelector(".note");
+// change.textContent = "change notes written";
+
+const welcome = document.querySelector(".navbox");
+// welcome.innerHTML = "<p> Welcome to Our Website</p>";
